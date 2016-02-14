3b6550ce5e09db57cd5e344b72dac0299e6b1b64
kody smith <kodysmith2010@gmail.com>
2016-02-14 04:08:53 -0800

    Update date.js

--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 Datez = function(dateString) {
 
                 this.dateString = dateString || undefined;
-
+                var dateObj = new Date(dateString);
                 var self = this,
                     beginning = "^",
                     ending = "$",
@@ -13,6 +13,9 @@ Datez = function(dateString) {
                     emptyYearPattern = "[0]{4}",
                     emptyMonthPattern = "[0]{2}",
                     emptyDayPattern = "[0]{2}";
+
+                this.days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+                this.months = ["January","Feburary","March","April","May","June","July","August","September","October","November","Decenber"];
 
                 this.datePatterns = {
 
@@ -96,6 +99,50 @@ Datez = function(dateString) {
                         self.month = dateArray[1];
                         self.year = dateArray[2];
                     }
+                };
+
+                this.toStringPatterns = {
+                    dd: function() {
+                        return self.day;
+                    },
+                    DD: function() {
+                        return self.days[dateObj.getDay()];
+                    },
+                    D: function() {
+                        return self.days[dateObj.getDay()].substr(0,3);
+                    },
+                    mm: function() {
+                        return this.month;
+                    },
+                    MM: function() {
+                        return self.months[dateObj.getMonth()];
+                    },
+                    M: function() {
+                        return self.months[dateObj.getMonth()].substr(0,3);
+                    },
+                    yyyy: function() {
+                        return self.year;
+                    }
+
+                };
+
+                function constructPattern(patternString) {
+                    var lcPattern = patternString.toLowerCase(),
+                            dayPattern,
+                            monthPattern,
+                            yearPattern,
+                            newPatternString;
+
+                    dayPattern = patternString.substr(lcPattern.indexOf('d'),(lcPattern.lastIndexOf('d') === 0)?1:lcPattern.lastIndexOf('d')-1);
+                    monthPattern = patternString.substr(lcPattern.indexOf('m'),(lcPattern.lastIndexOf('m') === 0)?1:lcPattern.lastIndexOf('m'));
+                    yearPattern = patternString.substr(lcPattern.indexOf('y'),(lcPattern.lastIndexOf('y') === 0)?1:lcPattern.lastIndexOf('y'));
+
+                    newPatternString = patternString.replace(dayPattern, self.toStringPatterns[dayPattern]() + ' ');
+                    newPatternString = newPatternString.replace(monthPattern, self.toStringPatterns[monthPattern]() + ' ');
+                    newPatternString = newPatternString.replace(yearPattern, self.toStringPatterns[yearPattern]());
+
+                    return newPatternString;
+
                 }
 
                 this.parseDate = function(dateString) {
@@ -120,6 +167,11 @@ Datez = function(dateString) {
                     }
                     return self.year, self.month, self.day;
                 }
+
+                this.toString = function(stringPattern) {
+
+                    return constructPattern(stringPattern);
+                };
 
                 this.parseDate();
             };
